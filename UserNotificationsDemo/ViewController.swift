006ef020fecbe3bd86b48a929eab63a0c4ece431
Shruti Sharma <shruti25sharma@gmail.com>
2018-03-01 17:04:39 -0800

    New commit

--- UserNotificationsDemo/ViewController.swift
+++ UserNotificationsDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         //1. REQUEST AUTHORIZATION FOR USER NOTIFICATIONS FROM USER
         /* we call the requestAuthorization method of the notification center to ask the user’s permission for using notification. We also request the ability to display alerts and play sounds. */
         
